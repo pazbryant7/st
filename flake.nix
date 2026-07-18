ea7ca21c5ff0c7d14648e9876fa41879fd29077c
Bryant Paz <pazbryant7@outlook.com>
2026-07-18 14:20:55 -0600

    feat(nix): new flake added

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "My st (simple terminal) build";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages.default = pkgs.st.overrideAttrs (old: {
+          src = ./.;
+        });
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            clang-tools
+            patch
+          ];
+        };
+        formatter = pkgs.nixfmt-tree;
+      }
+    );
+}
