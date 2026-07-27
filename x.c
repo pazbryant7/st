c1e4495837bc9f07e628258493eed1e2522f392e
Bryant Paz <pazbryant7@outlook.com>
2026-07-27 16:41:19 -0600

    feat(xinit): opt_font and font always null at start

--- x.c
+++ x.c
@@ -1284,7 +1284,7 @@ xinit(int cols, int rows)
 	if (!FcInit())
 		die("could not init fontconfig.\n");
 
-	usedfont = (opt_font == NULL)? fonts[currentfont] : opt_font;
+	usedfont = fonts[currentfont];
 	xloadfonts(usedfont, 0);
 
 	/* spare fonts */
